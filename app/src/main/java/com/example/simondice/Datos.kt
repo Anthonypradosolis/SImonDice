68fc87960c5b9498e9dec639e5d14e5783266d8a
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-11-27 16:28:49 +0100

    Creacion de una clase enum, para los colores de los botones

--- app/src/main/java/com/example/simondice/Datos.kt
+++ app/src/main/java/com/example/simondice/Datos.kt
@@ -1,5 +1,6 @@
 package com.example.simondice
 
+import androidx.compose.ui.graphics.Color
 import androidx.lifecycle.MutableLiveData
 
 object Datos {
@@ -7,4 +8,19 @@ object Datos {
     var numero: Int = 0
     // Se inicializa la variable ronda en 0
     var ronda : MutableLiveData<Int> = MutableLiveData(0)
+}
+
+
+/**
+ * Enumeración de los colores de los botones
+ * @param color Color del botón
+ * @param label Etiqueta del botón
+ * @param value Valor del botón
+ * @constructor Crea un objeto de la clase ColorButton
+ */
+enum class ColorButton(val color: Color, val label: String, val value: Int) {
+    VERDE(Color.Green, "Verde", 1),
+    ROJO(Color.Red, "Rojo", 2),
+    AMARILLO(Color.Yellow, "Amarillo", 3),
+    AZUL(Color.Blue, "Azul", 4)
 }
