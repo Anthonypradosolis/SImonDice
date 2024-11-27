d007ed1cf66dc4bed3cbcab6cf7a6154afba11ca
Anthonypradosolis <apradosolis@danielcastelao.org>
2024-11-27 16:27:38 +0100

    Cambiamos la clase Datos a object y creamos dos variables

--- app/src/main/java/com/example/simondice/Datos.kt
+++ app/src/main/java/com/example/simondice/Datos.kt
@@ -1,4 +1,10 @@
 package com.example.simondice
 
-class Datos {
+import androidx.lifecycle.MutableLiveData
+
+object Datos {
+    // Se inicializa la variable numero en 0
+    var numero: Int = 0
+    // Se inicializa la variable ronda en 0
+    var ronda : MutableLiveData<Int> = MutableLiveData(0)
 }
